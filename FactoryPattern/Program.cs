300170b730f80a73379d6337fcf0e3d7f2fc0451
kesdeband <bugs_626@hotmail.com>
2015-12-12 08:59:19 -0400

    Visual Studio First Commit

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using FactoryPattern.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = VehicleFactory.GetVehicle(VehicleFactory.VehicleType.Car);
+            Vehicle superCar = VehicleFactory.GetVehicle(VehicleFactory.VehicleType.SuperCar);
+            Vehicle truck = VehicleFactory.GetVehicle(VehicleFactory.VehicleType.Truck);
+            Vehicle motorCycle = VehicleFactory.GetVehicle(VehicleFactory.VehicleType.Motorcyle);
+            //Vehicle test = VehicleFactory.GetVehicle((VehicleFactory.VehicleType)100);
+            Console.Write(car + "\n");
+            Console.Write(superCar + "\n");
+            Console.Write(truck + "\n");
+            Console.Write(motorCycle + "\n");
+        }
+    }
+}
